bb930675db89cf49e5d3ee27010d1583f3ee159d
SuperCoac <levictor3000@hotmail.fr>
2015-06-18 14:52:56 +0200

    Finitions de la slot machine

--- games/1/js/game.js
+++ games/1/js/game.js
@@ -5,6 +5,13 @@ function preload() {
     game.load.image('button', 'assets/button.png');
     game.load.image('background','assets/bg.jpg');
 
+    game.load.image('1','assets/1.png');
+    game.load.image('2','assets/2.jpg');
+    game.load.image('3','assets/3.png');
+    game.load.image('4','assets/4.jpg');
+    game.load.image('5','assets/5.jpg');
+
+
 }
 
 var button;
@@ -15,7 +22,10 @@ var n1;
 var n2;
 var n3;
 var min_ = 1;
-var max_ = 9;
+var max_ = 4;
+var slot1;
+var slot2;
+var slot3;
 
 function create() {
 
@@ -26,7 +36,7 @@ function create() {
     background.height = 600;
 
 
-    button = game.add.sprite(game.world.centerX - 95, 400, 'button');
+    button = game.add.sprite(game.world.centerX, 500, 'button');
     button.width /= 2;
     button.height /= 2;
     button.anchor.set(0.5);
@@ -34,12 +44,32 @@ function create() {
     button.inputEnabled = true;
     button.events.onInputOver.add(over, this);
     button.events.onInputOut.add(out, this);
-    button.events.onInputUp.add(up, this);  
+    button.events.onInputUp.add(up, this); 
 
-    var style = { font: "65px Arial", fill: "#00ffff", align: "center" };
+
+    slot1 = game.add.sprite(game.world.centerX -200, game.world.centerY - 50, '1');
+    slot2 = game.add.sprite(game.world.centerX, game.world.centerY - 50, '2');
+    slot3 = game.add.sprite(game.world.centerX + 200, game.world.centerY - 50, '3');
+
+    slot1.anchor.set(0.5);
+    slot2.anchor.set(0.5);
+    slot3.anchor.set(0.5);
+
+    slot1.width = 150;
+    slot1.height = 150;
+    slot2.width = 150;
+    slot2.height = 150;
+    slot3.width = 150;
+    slot3.height = 150;
+
+
+
+
+    var style = { font: "65px Arial", fill: "#000000", align: "center" };
     text = game.add.text(game.world.centerX, game.world.centerY, "", style);
     text.anchor.set(0.5);
-    textWin = game.add.text(game.world.centerX, game.world.centerY + 200, "", style);
+    textWin = game.add.text(game.world.centerX, game.world.centerY - 200, "", style);
+    textWin.anchor.set(0.5);
 
 }
 
@@ -48,18 +78,23 @@ function up() {
     n1 = getRandomInt(min_, max_);
     n2 = getRandomInt(min_, max_);
     n3 = getRandomInt(min_, max_);
-    game.time.events.add(100, function(){ text.setText( getRandomInt(min_, max_) + " " + getRandomInt(min_, max_) + " " + getRandomInt(min_, max_)); } );
-    game.time.events.add(200, function(){ text.setText( getRandomInt(min_, max_) + " " + getRandomInt(min_, max_) + " " + getRandomInt(min_, max_)); } );
-    game.time.events.add(300, function(){ text.setText( getRandomInt(min_, max_) + " " + getRandomInt(min_, max_) + " " + getRandomInt(min_, max_)); } );
-    game.time.events.add(400, function(){ text.setText( getRandomInt(min_, max_) + " " + getRandomInt(min_, max_) + " " + getRandomInt(min_, max_)); } );
-    game.time.events.add(500, function(){ text.setText( getRandomInt(min_, max_) + " " + getRandomInt(min_, max_) + " " + getRandomInt(min_, max_)); } );
-    game.time.events.add(600, function(){ text.setText( getRandomInt(min_, max_) + " " + getRandomInt(min_, max_) + " " + getRandomInt(min_, max_)); } );
-    game.time.events.add(700, function(){ text.setText( getRandomInt(min_, max_) + " " + getRandomInt(min_, max_) + " " + getRandomInt(min_, max_)); } );
-    game.time.events.add(800, function(){ text.setText( getRandomInt(min_, max_) + " " + getRandomInt(min_, max_) + " " + getRandomInt(min_, max_)); } );
-    game.time.events.add(900, function(){ text.setText( getRandomInt(min_, max_) + " " + getRandomInt(min_, max_) + " " + getRandomInt(min_, max_)); } );
-    game.time.events.add(1000, 
+    game.time.events.add(100, function(){ slot1.loadTexture(getRandomInt(min_, max_) + ""); slot2.loadTexture(getRandomInt(min_, max_) + ""); slot3.loadTexture(getRandomInt(min_, max_) + "");} );
+    
+    game.time.events.add(200, function(){ slot1.loadTexture(getRandomInt(min_, max_) + ""); slot2.loadTexture(getRandomInt(min_, max_) + ""); slot3.loadTexture(getRandomInt(min_, max_) + "");} );
+    game.time.events.add(300, function(){ slot1.loadTexture(getRandomInt(min_, max_) + ""); slot2.loadTexture(getRandomInt(min_, max_) + ""); slot3.loadTexture(getRandomInt(min_, max_) + "");} );
+    game.time.events.add(400, function(){ slot1.loadTexture(getRandomInt(min_, max_) + ""); slot2.loadTexture(getRandomInt(min_, max_) + ""); slot3.loadTexture(getRandomInt(min_, max_) + "");} );
+    game.time.events.add(500, function(){ slot1.loadTexture(getRandomInt(min_, max_) + ""); slot2.loadTexture(getRandomInt(min_, max_) + ""); slot3.loadTexture(getRandomInt(min_, max_) + "");} );
+    game.time.events.add(600, function(){ slot1.loadTexture(getRandomInt(min_, max_) + ""); slot2.loadTexture(getRandomInt(min_, max_) + ""); slot3.loadTexture(getRandomInt(min_, max_) + "");} );
+    game.time.events.add(700, function(){ slot1.loadTexture(getRandomInt(min_, max_) + ""); slot2.loadTexture(getRandomInt(min_, max_) + ""); slot3.loadTexture(getRandomInt(min_, max_) + "");} );
+    game.time.events.add(800, function(){ slot1.loadTexture(getRandomInt(min_, max_) + ""); slot2.loadTexture(getRandomInt(min_, max_) + ""); slot3.loadTexture(getRandomInt(min_, max_) + "");} );
+    game.time.events.add(900, function(){ slot1.loadTexture(getRandomInt(min_, max_) + ""); slot2.loadTexture(getRandomInt(min_, max_) + ""); slot3.loadTexture(getRandomInt(min_, max_) + "");} );
+    game.time.events.add(1000, function(){ slot1.loadTexture(getRandomInt(min_, max_) + ""); slot2.loadTexture(getRandomInt(min_, max_) + ""); slot3.loadTexture(getRandomInt(min_, max_) + "");} );
+   
+    game.time.events.add(1100, 
         function(){ 
-            text.setText( n1 + " " + n2 + " " + n3); 
+            slot1.loadTexture(n1 + "");
+            slot2.loadTexture(n2 + "");
+            slot3.loadTexture(n3 + "");
             if(n1 == n2 && n1 == n3) 
                 textWin.setText("GAGNE");
             else
